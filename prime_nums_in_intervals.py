515724ec113abf49ae1a873fda75a04d43d6d273
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-12 14:58:16 +0530

    Update prime_nums_in_intervals.py

--- prime_nums_in_intervals.py
+++ prime_nums_in_intervals.py
@@ -1,10 +1,9 @@
-l,u=raw_input().split()
-lowe=int(l)
-uppe=int(u)
-for num in range(lowe,uppe):
+l,k=map(int,input().split())
+for num in range(l,k+ 1):
+   # prime numbers are greater than 1
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print num,
+           print(num)
