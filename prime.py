17e0d115fae6655241ee8e602a121fa438feb905
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-12 14:22:31 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,13 +1,14 @@
 import math 
-def check(n): 
+def checks(n): 
     if n == 1: 
         return False
     for x in range(2, (int)(math.sqrt(n))+1): 
         if n % x == 0: 
             return False 
     return True
-n = int(input())
-if check(n): 
+n = input()
+n=int(n)
+if checks(n): 
     print("yes")  
 else: 
     print("no") 
