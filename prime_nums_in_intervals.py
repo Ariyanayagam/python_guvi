8edba6f41269f09ea2b9db44eb1c4b165de94941
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-12 14:48:41 +0530

    Update prime_nums_in_intervals.py

--- prime_nums_in_intervals.py
+++ prime_nums_in_intervals.py
@@ -1,8 +1,8 @@
-l,u=map(int,input().split())
-for num in range(l,u):
-   if num > 1:
-       for i in range(2,num/2):
-           if (num % i) == 0:
-               break
-       else:
-           print (num,)
+b,t=map(int,input().split())
+for l in range(b,t+1):
+  u=0
+  for j in range(1,t):
+    if l%j==0:
+      u+=1
+  if u==2:
+    print(l,end=" ")
