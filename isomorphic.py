8fd5b00b63ac7b50a99ea9a526ef760a3820d040
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-19 12:30:33 +0530

    Update isomorphic.py

--- isomorphic.py
+++ isomorphic.py
@@ -1,7 +1,7 @@
 s11, s2 = raw_input().split()
 count = 0
-for i in range(len(s1)):
-  if s11.count(s1[i]) == s2.count(s2[i]):
+for i in range(len(s11)):
+  if s11.count(s11[i]) == s2.count(s2[i]):
     count += 1
 if count == len(s11):
   print "yes"
