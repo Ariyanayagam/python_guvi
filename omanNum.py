f9187b98aa46fb9e50146afaa35719a14c6de104
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-19 12:23:06 +0530

    Create omanNum.py

---
+++ omanNum.py
@@ -0,0 +1,5 @@
+lm1a=input()
+lm2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(lm1a==lm2[i]):
+    print(i)
