7dfb52e70635b177f0c9f14e5b2e6add076f68c0
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-19 12:38:47 +0530

    Update swapOddEvenChar.py

--- swapOddEvenChar.py
+++ swapOddEvenChar.py
@@ -1,8 +1,6 @@
-as = list(input())
-s1=''
-for i in range(0,len(as),2):
-  t=as[i]
-  as[i]=as[i+1]
-  as[i+1]=t
-  s1=s1+as[i]+as[i+1]
-print(s1)
+x=input()
+for i in range(len(x)):
+  if (i%2==0):
+    print(x[i+1],end='')
+  else:
+    print(x[i-1],end='')
