95f6f5417a7045c68a6f49e80aae7e5857d6d0b5
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-20 10:44:49 +0530

    Update rightShift.py

--- rightShift.py
+++ rightShift.py
@@ -1,6 +1,6 @@
 sx = [int(x) for x in input().split()]
 s1 = [int(x) for x in input().split()]
 s2=''
-for i in range(0,s[1]):
+for i in range(0,sx[1]):
   s1 = [s1[-1]] + s1[:-1]
 print(*s1,sep=' ')
