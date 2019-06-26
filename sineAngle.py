77568b128bfdad685415ed6404159c6d26d489d3
Ariyanayagam <51635641+Ariyanayagam@users.noreply.github.com>
2019-06-26 16:03:36 +0530

    Update sineAngle.py

--- sineAngle.py
+++ sineAngle.py
@@ -1,4 +1,8 @@
-import math
-pz=int(input())
-s=math.radians(pz)
-print(round(math.sin(s),1))
+import math 
+dx=float(input())
+m1=math.radians(dx)
+n=math.sin(m1)
+if(m1<1):
+    print(round(n,1))
+elif(m1>1):
+    print(round(n))
